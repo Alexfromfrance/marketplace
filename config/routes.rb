7bd1e7f76abf1f37b9af920155fad588374c98f4
alexandre auriol <auriol.alexandre@gmail.com>
2019-08-12 09:11:04 +0200

    restaurant model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  get 'contact', to: 'pages#contact', as: :contact
+
+  get 'about', to: 'pages#about', as: :about
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'restaurants', to: 'restaurants#index'
+
+  get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+
+  post 'restaurants', to: 'restaurants#create'
+
+  root to: 'pages#home'
 end
