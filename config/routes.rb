90115da3968b24a37aa977bcb3ff6ed4e52c6834
alexandre auriol <auriol.alexandre@gmail.com>
2019-08-12 12:15:50 +0200

    exo end

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,21 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about', as: :about
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get 'restaurants', to: 'restaurants#index'
+  resources :restaurants
 
-  get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+  # get 'restaurants', to: 'restaurants#index'
 
-  post 'restaurants', to: 'restaurants#create'
+  # get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+
+  # get 'restaurants/new', to: 'restaurants#new'
+  # post 'restaurants', to: 'restaurants#create'
+
+  # get 'restaurants/:id/edit', to: 'restaurants#edit'
+  # patch 'restaurants/:id', to: 'restaurants#update'
+
+  # delete 'restaurants/:id', to: 'restaurants#destroy'
+
+
 
   root to: 'pages#home'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.create(name:"chezbob", address:"paris", category:"fr", stars:2)
+Restaurant.create(name:"ledindon", address:"paris", category:"gastro", stars:3)
